35347937ba95556645bcf191bf2e05f160634a06
Veronica Cary <veronica.cary@state.mn.us>
2017-08-03 15:16:39 -0500

    Authenticating access using auth key

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is an extremely simple notifier for the MN-DHS BlueZone Scripts project, built on Electron and using the GitHub API.
 
 It is currently in development. When released it will be announced by Veronica Cary.
+
+## Credits
+
+Primary development by Veronica Cary, of the Minnesota Department of Human Services. Project utilizes the [node-github](https://github.com/mikedeboer/node-github) node.js client, which is licensed under the [MIT license](https://github.com/mikedeboer/node-github/blob/master/LICENSE). 
+
+As a work of an employee of the State of Minnesota, all other code is considered public domain, and provided here as-is, without warrantee.
--- renderer.js
+++ renderer.js
@@ -11,5 +11,40 @@ function notificationTest() {
     }
 }
 
+function clickTest() {
+  var queryColumnNumber = "1326504";
+  
+  var queryToken = document.getElementById("queryTokenInput").value;
+
+
+  var GitHubApi = require("github");
+
+  var github = new GitHubApi({
+    // optional
+    debug: false,
+    protocol: "https",
+    host: "api.github.com", // should be api.github.com for GitHub
+    headers: {
+      "Accept": "application/vnd.github.inertia-preview+json"
+    },
+    //username: queryUser,
+    Promise: require('bluebird'),
+    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+    timeout: 5000
+  });
+
+  // oauth
+  github.authenticate({
+    type: "oauth",
+    token: queryToken
+  });
+
+  github.projects.getProjectCards({
+  column_id: queryColumnNumber
+  }, function(err, res) {
+      document.write(JSON.stringify(res));
+  });
+}
+
 // This bit of jQuery listens for the button click then executes it!
 document.querySelector('#testButton').addEventListener('click', clickTest);
