6492236191d815e3252fd6b33d8cf12b99ab64ba
Veronica Cary <veronica.cary@state.mn.us>
2017-08-08 17:42:50 -0500

    #1: linted all code
    Removed semicolons, standardized formatting, etc.. Using ES-Lint, so added appropriate definitions files.

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // --- This file creates the main processes used by our application!
 
 // Constants which we'll need which require electron
-const {app, Tray, Menu, shell, BrowserWindow} = require('electron');
+const {app, Tray, Menu, BrowserWindow} = require("electron")
 
 // Defining a path and a URL
-const path = require('path')
-const url = require('url')
-const Store = require('./js/store.js');
+const path = require("path")
+const url = require("url")
+const Store = require("./js/store.js")
 
 // Sets up the icons for the taskbar and main app
-const trayIconPath = path.join(__dirname, 'icon-default-16.png');
-const appIconPath = path.join(__dirname, 'icon-default-256.png');
+const trayIconPath = path.join(__dirname, "icon-default-16.png")
+const appIconPath = path.join(__dirname, "icon-default-256.png")
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -18,99 +18,99 @@ let mainWindow
 
 // Instantiate the class for setting window defaults
 const store = new Store({
-  // We'll call our data file 'user-preferences'
-  configName: 'user-preferences',
-  defaults: {
-    // 400x600 is the default size of our window
-    windowBounds: { width: 400, height: 750 },
-  }
-});
+	// We'll call our data file 'user-preferences'
+	configName: "user-preferences"
+	, defaults: {
+		// 400x600 is the default size of our window
+		windowBounds: { width: 400, height: 750 }
+	}
+})
 
 // This function creates the browser window which we'll use for main setup on starting
 function createWindow () {
-  // Create the browser window.
-  
-  // First we'll get our height and width. This will be the defaults if there wasn't anything saved
-  let { width, height } = store.get('windowBounds');
-  
-  mainWindow = new BrowserWindow({
-    width,
-    height,
-    icon: appIconPath
-  })
-  
-  // The BrowserWindow class extends the node.js core EventEmitter class, so we use that API
-  // to listen to events on the BrowserWindow. The resize event is emitted when the window size changes.
-  mainWindow.on('resize', () => {
-    // The event doesn't pass us the window size, so we call the `getBounds` method which returns an object with
-    // the height, width, and x and y coordinates.
-    let { width, height } = mainWindow.getBounds();
-    // Now that we have them, save them using the `set` method.
-    store.set('windowBounds', { width, height });
-  });
+	// Create the browser window.
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-  
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+	// First we'll get our height and width. This will be the defaults if there wasn't anything saved
+	let { width, height } = store.get("windowBounds")
+
+	mainWindow = new BrowserWindow({
+		width
+		, height
+		, icon: appIconPath
+	})
+
+	// The BrowserWindow class extends the node.js core EventEmitter class, so we use that API
+	// to listen to events on the BrowserWindow. The resize event is emitted when the window size changes.
+	mainWindow.on("resize", () => {
+		// The event doesn't pass us the window size, so we call the `getBounds` method which returns an object with
+		// the height, width, and x and y coordinates.
+		let { width, height } = mainWindow.getBounds()
+		// Now that we have them, save them using the `set` method.
+		store.set("windowBounds", { width, height })
+	})
+
+	// and load the index.html of the app.
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, "index.html")
+		, protocol: "file:"
+		, slashes: true
+	}))
+
+	// Emitted when the window is closed.
+	mainWindow.on("closed", function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null
+	})
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow)
 
 // ------ This stuff is ALL for the tray icon!!!
-app.on('ready', function(){
-  // Create new BrowserWindow object for the tray icon, so it runs in the background.
-  // As such "show" is set to false.
-  win = new BrowserWindow({show: false});
-  
-  // Defining the app icon
-  appIcon = new Tray(trayIconPath);
-  
-  // This sets up the contextMenu which will be used to feed the menu to the user
-  var contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'View your subscribed changes'
-    },
-    {
-      label: 'View PRISM scripts on GitHub',
-      click () { require('electron').shell.openExternal('https://github.com/MN-Script-Team/DHS-PRISM-Scripts/') }
-    },
-    { type: 'separator' },
-    { 
-      label: 'Exit',
-      selector: 'terminate:',
-    }
-  ]);
-  appIcon.setToolTip('BlueZone Scripts Notifications');
-  appIcon.setContextMenu(contextMenu);
-});
+app.on("ready", function () {
+	// Create new BrowserWindow object for the tray icon, so it runs in the background.
+	// As such "show" is set to false.
+	var win = new BrowserWindow({show: false})
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	// Defining the app icon
+	var appIcon = new Tray(trayIconPath)
+
+	// This sets up the contextMenu which will be used to feed the menu to the user
+	var contextMenu = Menu.buildFromTemplate([
+		{
+			label: "View your subscribed changes"
+		}
+		, {
+			label: "View PRISM scripts on GitHub"
+			, click () { require("electron").shell.openExternal("https://github.com/MN-Script-Team/DHS-PRISM-Scripts/") }
+		}
+		, { type: "separator" }
+		, {
+			label: "Exit"
+			, selector: "terminate:"
+		}
+	])
+	appIcon.setToolTip("BlueZone Scripts Notifications")
+	appIcon.setContextMenu(contextMenu)
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== "darwin") {
+		app.quit()
+	}
+})
+
+app.on("activate", function () {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow()
+	}
 })
--- renderer.js
+++ renderer.js
@@ -2,11 +2,11 @@
 // process for that window. All of the Node.js APIs are available in this process.
 
 // ---------------------------------------------------------------------------- DEFINING CONSTANTS
-const SHELL = require('electron').shell;										// Required by process to open links in default browser
+const SHELL = require("electron").shell											// Required by process to open links in default browser
 
 // ---------------------------------------------------------------------------- DEFINING CLASSES
-var FS_storage_connection = require('./js/store.js');							// Filesystem storage/retrieval 
-var GitHub_API_connection = require("github");									// Required by all GitHub API calls
+var FS_storage_connection = require("./js/store.js")							// Filesystem storage/retrieval 
+var GitHub_API_connection = require("github")									// Required by all GitHub API calls
 
 // ---------------------------------------------------------------------------- DEFINING GLOBAL VARIABLES/OBJECTS
 
@@ -18,227 +18,178 @@ var objGitHub = new GitHub_API_connection({
 	, headers: {
 		"Accept": "application/vnd.github.inertia-preview+json"					// Required because the projects API is still "experimental"
 	}
-	, Promise: require('bluebird') 												// Not totally sure why this is used ATM
+	, Promise: require("bluebird") 												// Not totally sure why this is used ATM
 	, followRedirects: false 													// default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
 	, timeout: 5000
-});
+})
 
 // Define a new object which stores the info located in the json file, if it
 // doesn't exist, load a default blank key
 var objKeyFS = new FS_storage_connection({
-	configName: 'user-login-info'
+	configName: "user-login-info"
 	, defaults: {
 		githubKey: ""
 	}
-});
+})
 
 // This variable contains the GitHub Key used by multiple functions
-var globalGitHubKey = null;
-
-// This variable defines whether-or-not a valid key is entered, defaults to false
-var validGitHubKey = false;
-
-
+var globalGitHubKey = null
 
 // ---------------------------------------------------------------------------- DEFINING FUNCTIONS
 
 // This gets changes from GitHub
-function getGitHubChanges() {
-	
+function getGitHubChanges () {
+
 	// This function reads all cards in a column and updates the list-item's badge 
-	function addColumnTotals(id) {		
+	function addColumnTotals (id) {
 		objGitHub.projects.getProjectCards({
 			column_id: id
-		}, function(err, res) {
-			$("#badge" + id).text(res.data.length);
-		});
-	};
+		}, function (err, res) {
+			$("#badge" + id).text(res.data.length)
+		})
+	}
 
 	// This function reads all of the columns in a project and adds it to the list
-	function addColumnsToList(id, name) {
+	function addColumnsToList (id, name) {
 		$("#changesList").append(
-			"<button class='list-group-item' id='col" + id + "'>" 
+			"<button class='list-group-item' id='col" + id + "'>"
 				+ "\n\t<span class='badge' id='badge" + id + "'></span>"
 				+ "\n\t" + name
 			+ "</button>"
-		);
-	};
-  
+		)
+	}
+
 	// Now lets get the data. First we get repo projects, then the specific project 
 	// details, then the columns, then the cards. 
 	objGitHub.projects.getRepoProjects({
-		owner: "MN-Script-Team",
-		repo: "DHS-PRISM-Scripts"
-	}, function(err, res) {
-	
+		owner: "MN-Script-Team"
+		, repo: "DHS-PRISM-Scripts"
+	}, function (err, res) {
+
 		// Log the JSON string for debugging purposes
-		console.log("getRepoProjects: " + JSON.stringify(res));
-	
+		console.log("getRepoProjects: " + JSON.stringify(res))
+
 		// Grab the ID from the first project!
-		var projectID = res.data[0].id; 
-	
+		var projectID = res.data[0].id
+
 		// Now we need to go through the project and grab info from each column!
 		objGitHub.projects.getProjectColumns({
 			project_id: projectID
-		}, function(err, res) {
-	  
+		}, function (err, res) {
+
 			// Log the JSON string for debugging purposes
-			console.log("getProjectColumns: " + JSON.stringify(res));
-	  
+			console.log("getProjectColumns: " + JSON.stringify(res))
+
 			// Clear out any existing list items adds a refresh button to the top
 			$("#changesList").html(
 				"<button class='list-group-item active' id='refreshChanges'>"
 					+ "\n\tRefresh this list!"
 				+ "\n </button>"
-			);
-			
-			$("#refreshChanges").on("click", function() {validateGitHubKey();});// Listening for the click to send the API token for authentication
-	  
-			// Now we go through each column (res.data reads this as an array), and update the display!
+			)
+
+			// Listening for the click to send the API token for authentication
+			$("#refreshChanges").on("click", function () {
+				validateGitHubKey()
+			})
+
+			// Now we go through each column and update the display!
 			for (var x in res.data) {
 				if (res.data.hasOwnProperty(x)) {
-					console.log(JSON.stringify(res.data[x]));					// Log the JSON string for debugging purposes
+					console.log(JSON.stringify(res.data[x]))					// Log the JSON string for debugging purposes
 					addColumnsToList(res.data[x].id, res.data[x].name)			// Add these columns to the list
-					addColumnTotals(res.data[x].id);							// Collects/displays the total cards in that column
-				};
-			};
-		});    
-	});
-};
+					addColumnTotals(res.data[x].id)								// Collects/displays the total cards in that column
+				}
+			}
+		})
+	})
+}
 
-// This checks the GitHub key entered and sees if it's accurate. If it works it'll update the UI and then check for updates
-function validateGitHubKey() {
-  
+// This checks the GitHub key entered and sees if it's accurate. If it works 
+// it'll update the UI and then check for updates
+function validateGitHubKey () {
+
 	// OAuth login using user-provided token
 	objGitHub.authenticate({
 		type: "oauth"
 		, token: globalGitHubKey
-	});
-  
+	})
+
 	// Get and display user details from GitHub
-	objGitHub.users.get({}, function(err, res) {
+	objGitHub.users.get({}, function (err, res) {
 
 		// Error handling block
-		if(err) {
-			
+		if (err) {
+
 			// If you aren't signed in it'll let you know!
 			$("#alertZone").html(
 				"<div class='alert alert-danger' role='alert'>"
 					+ "\n\t <strong>Oh no!</strong> Invalid token!"
 				+ "</div>"
-			);
+			)
 
 			// Defines the GitHub Key as invalid, then exits the function
-			return validGitHubKey = false;
+			// return validGitHubKey = false
 		//	return;
-		};
-		
+		}
+
 		// Creates a signed-in alert
 		$("#alertZone").html(
 			"<div class='alert alert-success' role='alert'>"
 				+ "\n\t<img id='avatarGitHub' class='img-responsive img-circle'"
-				+ " src='" + res.data.avatar_url + "' />" 
-				+ "\n\t Signed in as <a class='alert-link' href='" 
+				+ " src='" + res.data.avatar_url + "' />"
+				+ "\n\t Signed in as <a class='alert-link' href='"
 				+ res.data.html_url + "'>" + res.data.login + "</a>!"
 			+ "</div>"
-		);
+		)
 
-		
 		// Get rate limit, may use in the future to warn users who are too close
-		objGitHub.misc.getRateLimit({}, function(err, res) {
-			console.log(res.data.resources.core.remaining);
-		});
-		
-		
-		getGitHubChanges();														// Initial check now that we're authenticated
-		setInterval(function(){getGitHubChanges(); }, 600000);					// Check GitHub for content of project columns every 10 minutes (10 * 60 * 1000)
-		return validGitHubKey = true;											// Defines the GitHub Key as valid
-	});
-  
+		objGitHub.misc.getRateLimit({}, function (err, res) {
+			console.log(res.data.resources.core.remaining)
+		})
+
+		getGitHubChanges()														// Initial check now that we're authenticated
+		setInterval(function () { getGitHubChanges() }, 600000)					// Check GitHub for content of project columns every 10 minutes (10 * 60 * 1000)
+		// return validGitHubKey = true											// Defines the GitHub Key as valid
+	})
 }
 
+/*
+function notificationTest () {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function notificationTest() {
-  
-	let myNotification = new Notification('Title', {
-		body: 'Lorem Ipsum Dolor Sit Amet'
+	let myNotification = new Notification("Title", {
+		body: "Lorem Ipsum Dolor Sit Amet"
 	})
 
 	myNotification.onclick = () => {
-		console.log('Notification clicked')
+		console.log("Notification clicked")
 	}
 }
-
-
-
-
-
-
-
-
-
+*/
 
 // ---------------------------------------------------------------------------- EVENT LISTENERS
 
-$(document).ready(function() {
-	
-	// Grabs the GitHub key from the JSON file on load, and validates it
-	$(document).ready(function() {
-		globalGitHubKey = objKeyFS.get('githubKey');							// Set the variable from the JSON
-		$("#queryTokenInput").val(globalGitHubKey);								// Display the key in the input
-		validateGitHubKey();													// Validate via GitHub
-	});
-	
-	// Listening for the sendToken click to send the API token for authentication
-	$("#sendToken").on("click", function() {
-		globalGitHubKey = $("#queryTokenInput").val();							// Grab value from input
-		objKeyFS.set('githubKey', globalGitHubKey);								// Write the variable to the JSON
-		validateGitHubKey();													// Validate via GitHub						
-	});
-	
-	
-	
-	
-	// Force external links to load in default browser
-	$(document).on('click', 'a[href^="http"]', function(event) {
-		event.preventDefault();													// Shut down the default browser
-		SHELL.openExternal(this.href);											// SHELL is defined above
-	});
-	
-});
+$(document).ready(function () {
 
+	// Grabs the GitHub key from the JSON file on load, and validates it
+	$(document).ready(function () {
+		globalGitHubKey = objKeyFS.get("githubKey")								// Set the variable from the JSON
+		$("#queryTokenInput").val(globalGitHubKey)								// Display the key in the input
+		validateGitHubKey()														// Validate via GitHub
+	})
+
+	// Listening for the sendToken click to send the API token for authentication
+	$("#sendToken").on("click", function () {
+		globalGitHubKey = $("#queryTokenInput").val()							// Grab value from input
+		objKeyFS.set("githubKey", globalGitHubKey)								// Write the variable to the JSON
+		validateGitHubKey()														// Validate via GitHub						
+	})
+
+	// Force external links to load in default browser
+	$(document).on("click", "a[href^='http']", function (event) {
+		event.preventDefault()													// Shut down the default browser
+		SHELL.openExternal(this.href)											// SHELL is defined above
+	})
+
+})
 
 // ---------------------------------------------------------------------------- LOOPING TO RECHECK API FOR CHANGES
-
-
-// 
-
-
-//	validateGitHubKey();
-//	if (validGitHubKey) {getGitHubChanges();};								// Check GitHub for content of project columns
